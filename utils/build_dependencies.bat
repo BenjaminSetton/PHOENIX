5e7eb12e2f9ed0a8d5063192d6bdc03082932c7e
BenjaminSetton <benjasetton@gmail.com>
2025-01-13 22:52:12 -0600

    Started working to build shaderc dependency

--- utils/build_dependencies.bat
+++ utils/build_dependencies.bat
@@ -3,10 +3,8 @@
 call config.bat
 
 :: Build GLFW
-echo Building GLFW dependency...
-set GLFW_BUILD_DIR=%WORKSPACE_DIR%PHOENIX\out\glfw
-set GLFW_BUILD_DIR_DEB="%GLFW_BUILD_DIR%\debug"
-set GLFW_BUILD_DIR_REL="%GLFW_BUILD_DIR%\release"
+echo [PHOENIX] Building GLFW dependency...
+::set GLFW_BUILD_DIR=%WORKSPACE_DIR%PHOENIX\out\glfw
 
 ::echo workspace_dir - "%WORKSPACE_DIR%"
 ::echo build dir - "%GLFW_BUILD_DIR%"
@@ -14,16 +12,36 @@ set GLFW_BUILD_DIR_REL="%GLFW_BUILD_DIR%\release"
 ::echo build_dir_rel - "%GLFW_BUILD_DIR_REL%"
 
 cd %WORKSPACE_DIR%\PHOENIX\vendor\glfw
-if not exist "%GLFW_BUILD_DIR%" mkdir "%GLFW_BUILD_DIR%"
+::if not exist "%GLFW_BUILD_DIR%" mkdir "%GLFW_BUILD_DIR%"
 cmake -S . -B build
 cd build
 
-echo Started building GLFW debug...
+echo [PHOENIX] Started building GLFW debug...
 cmake --build . --config Debug
-echo Finished building GLFW debug!
+echo [PHOENIX] Finished building GLFW debug!
 
-echo Started building GLFW release...
+echo [PHOENIX] Started building GLFW release...
 cmake --build . --config Release
-echo Finished building GLFW release!
+echo [PHOENIX] Finished building GLFW release!
 
-echo Finished building GLFW!
+echo [PHOENIX] Finished building GLFW!
+
+:: Build shaderc
+echo [PHOENIX] Building ShaderC dependency...
+
+cd %WORKSPACE_DIR%\PHOENIX\vendor\shaderc-phx
+cmake -S . -B build -DSHADERC_SKIP_EXAMPLES=ON -DSHADERC_SKIP_TESTS=ON
+cd build
+
+echo [PHOENIX] Started building ShaderC debug...
+cmake --build . --config Debug
+echo [PHOENIX] Finished building ShaderC debug!
+
+echo [PHOENIX] Started building ShaderC release...
+cmake --build . --config Release
+echo [PHOENIX] Finished building ShaderC release!
+
+echo [PHOENIX] Finished building ShaderC!
+
+echo [PHOENIX] Finished building dependencies!
+pause
